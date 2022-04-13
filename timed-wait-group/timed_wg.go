f45c58e83965f6379bdd2dbcf6e1ebce9dbee745
Stanislav Bulatkin <sbulatkin@b2broker.net>
2022-04-13 10:33:55 +0300

    init: Time Wait Group

---
+++ timed-wait-group/timed_wg.go
@@ -0,0 +1,36 @@
+package utils
+
+import (
+	"errors"
+	"sync"
+	"time"
+)
+
+// ErrWGExpired is returned if the timeout expires before the WaitGroup has received
+// a "Done" confirmation from all the streams.
+var ErrWGExpired = errors.New("wait group timeout expired")
+
+// TimedWaitGroup is a combination of WaitGroup and timeout
+type TimedWaitGroup struct {
+	sync.WaitGroup
+}
+
+// WaitWithTimeout awaits any of the two cases:
+// - timeout expires (in which case an error is returned)
+// - a WaitGroup receives a "Done" confirmation (in which case nil is returned)
+func (wg *TimedWaitGroup) WaitWithTimeout(timeout time.Duration) error {
+	ch := make(chan struct{})
+
+	go func() {
+		defer close(ch)
+		wg.Wait()
+	}()
+
+	select {
+	case <-ch:
+		return nil
+
+	case <-time.After(timeout):
+		return ErrWGExpired
+	}
+}
